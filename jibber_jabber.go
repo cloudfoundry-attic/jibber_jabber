eaec3ca988560c6aa6f6066517ce91ead6b1abae
Anand Gaitonde and Michael Maximilien <maximilien@pivotallabs.com>
2014-05-28 17:00:09 -0700

    Adding windows code.

--- jibber_jabber.go
+++ jibber_jabber.go
@@ -1,5 +1,17 @@
 package Jibber_Jabber
 
-const (
-  COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE = "Could not detect Langauge"
+import (
+	"strings"
 )
+
+const (
+	COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE = "Could not detect Language"
+)
+
+func splitLocale(locale string) (string, string) {
+	formattedLocale := strings.Split(locale, ".")[0]
+	formattedLocale = strings.Replace(formattedLocale, "-", "_", -1)
+	language := strings.Split(formattedLocale, "_")[0]
+	territory := strings.Split(formattedLocale, "_")[1]
+	return language, territory
+}
--- unix.go
+++ unix.go
@@ -2,61 +2,53 @@
 package Jibber_Jabber
 
 import (
-  "os"
-  "strings"
-  "errors"
+	"errors"
+	"os"
+	"strings"
 )
 
 func getLangFromEnv() (locale string) {
-  locale = os.Getenv("LC_ALL")
-  if locale == "" {
-    locale = os.Getenv("LANG")
-  }
+	locale = os.Getenv("LC_ALL")
+	if locale == "" {
+		locale = os.Getenv("LANG")
+	}
 
-  return
+	return
 }
 
 func getUnixLocale() (unix_locale string, err error) {
-  unix_locale = getLangFromEnv()
-  if unix_locale == "" {
-    err = errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE)
-  }
+	unix_locale = getLangFromEnv()
+	if unix_locale == "" {
+		err = errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE)
+	}
 
-  return
-}
-
-func splitLocale(locale string) (string, string) {
-	formattedLocale := strings.Split(locale, ".")[0]
-	formattedLocale = strings.Replace(formattedLocale, "-", "_", -1)
-	language := strings.Split(formattedLocale, "_")[0]
-	territory := strings.Split(formattedLocale, "_")[1]
-	return language, territory
+	return
 }
 
 func DetectIETF() (locale string, err error) {
-  unix_locale, err := getUnixLocale()
-  if err == nil {
-    language, territory := splitLocale(unix_locale)
-    locale = language + "-" + territory
-  }
+	unix_locale, err := getUnixLocale()
+	if err == nil {
+		language, territory := splitLocale(unix_locale)
+		locale = language + "-" + territory
+	}
 
-  return
+	return
 }
 
 func DetectLanguage() (language string, err error) {
-  unix_locale, err := getUnixLocale()
-  if err == nil {
-    language, _ = splitLocale(unix_locale)
-  }
+	unix_locale, err := getUnixLocale()
+	if err == nil {
+		language, _ = splitLocale(unix_locale)
+	}
 
-  return
+	return
 }
 
 func DetectTerritory() (territory string, err error) {
-  unix_locale, err := getUnixLocale()
-  if err == nil {
-    _, territory = splitLocale(unix_locale)
-  }
+	unix_locale, err := getUnixLocale()
+	if err == nil {
+		_, territory = splitLocale(unix_locale)
+	}
 
-  return
+	return
 }
