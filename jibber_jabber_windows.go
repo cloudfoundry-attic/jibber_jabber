5cc90ecbb2bea0bea66a4d1f00347a0fed058d2b
fffw <fffw@getlantern.org>
2015-02-12 22:56:10 +0800

    use GetLocaleInfo instead

--- jibber_jabber_windows.go
+++ jibber_jabber_windows.go
@@ -9,6 +9,11 @@ import (
 )
 
 const LOCALE_NAME_MAX_LENGTH uint32 = 85
+// https://msdn.microsoft.com/en-us/library/windows/desktop/dd373848(v=vs.85).aspx
+const LOCALE_SISO_NAME_MAX_LENGTH uint32 = 9
+// defined in winnls.h
+const LOCALE_SISO639LANGNAME uint32 = 0x59
+const LOCALE_SISO3166CTRYNAME uint32 = 0x5a
 
 func getWindowsLocaleFrom(sysCall string) (locale string, err error) {
 	buffer := make([]uint16, LOCALE_NAME_MAX_LENGTH)
@@ -41,18 +46,24 @@ func getAllWindowsLocaleFrom(sysCall string) (string, error) {
 	if locale == 0 {
 		return "", errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE + ":\n" + dllError.Error())
 	}
-	proc, err = dll.FindProc("LCIDToLocaleName")
+	proc, err = dll.FindProc("GetLocaleInfo")
 	if err != nil {
 		return "", err
 	}
-	buffer := make([]uint16, LOCALE_NAME_MAX_LENGTH)
-	r, _, dllError := proc.Call(locale, uintptr(unsafe.Pointer(&buffer[0])), uintptr(LOCALE_NAME_MAX_LENGTH), 0)
+	langBuf := make([]uint16, LOCALE_SISO_NAME_MAX_LENGTH)
+	r, _, dllError := proc.Call(locale, uintptr(LOCALE_SISO639LANGNAME), uintptr(unsafe.Pointer(&langBuf[0])), uintptr(LOCALE_SISO_NAME_MAX_LENGTH))
+	if r == 0 {
+		err = errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE + ":\n" + dllError.Error())
+		return "", err
+	}
+	countryBuf := make([]uint16, LOCALE_SISO_NAME_MAX_LENGTH)
+	r, _, dllError = proc.Call(locale, uintptr(LOCALE_SISO3166CTRYNAME), uintptr(unsafe.Pointer(&langBuf[0])), uintptr(LOCALE_SISO_NAME_MAX_LENGTH))
 	if r == 0 {
 		err = errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE + ":\n" + dllError.Error())
 		return "", err
 	}
 	
-	return syscall.UTF16ToString(buffer), nil
+	return syscall.UTF16ToString(langBuf) + "-" + syscall.UTF16ToString(countryBuf), nil
 }
 
 func getWindowsLocale() (locale string, err error) {
